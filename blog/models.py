a31c5a413e823a8367ecddd245c94dcfaffa5d87
SushanGhimire <sushangmi55@gmail.com>
2021-05-21 13:08:58 +0545

    blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(("Title"), max_length=250)
+    content = models.TextField(("Content"))
+    created_date = models.DateTimeField(("Created Date"), auto_now_add=True)
+    published_date = models.DateTimeField(("Published Date"), auto_now=True)
+
+    def __str__(self):
+        return "{}".format(self.title)
+
+    # image = models.ImageField(
+    #     ("Image"), upload_to="post/images/", null=True, blank=True)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse;
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
+
 def index(request):
-    return render(request,'blog/index.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})
     # return HttpResponse("Hello World")
