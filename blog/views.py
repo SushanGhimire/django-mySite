7ee72ae377bc920b77c5365f9c8ba71f6ac59a45
SushanGhimire <sushangmi55@gmail.com>
2021-05-24 12:34:34 +0545

    blog filtered added

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-# Create your views here.
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by("-id")[:3]
     return render(request, 'blog/index.html', {'posts': posts})
-    # return HttpResponse("Hello World")
+
+
+def blogList(request):
+    AllPosts = Post.objects.all()
+    return render(request, 'blog/blog-list.html', {'AllPosts': AllPosts})
